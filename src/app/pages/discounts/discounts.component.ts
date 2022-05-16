fd74f577c225eab4c3348053bb241f69cb6bed5c
KarinaPryv <karinusha5@gmail.com>
2022-05-16 14:35:33 +0200

    added discounts page

--- src/app/pages/discounts/discounts.component.ts
+++ src/app/pages/discounts/discounts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DiscountResponseModel } from 'src/app/shared/models/discount.model';
+import { DiscountsService } from 'src/app/shared/services/discounts.service';
 
 @Component({
   selector: 'app-discounts',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscountsComponent implements OnInit {
 
-  constructor() { }
+  public discounts = new Array<DiscountResponseModel>();
+
+  constructor(private discountService: DiscountsService,) { }
 
   ngOnInit(): void {
+    this.discountService.getAll().subscribe(discounts => {
+      this.discounts = discounts;
+    })
   }
 
 }
